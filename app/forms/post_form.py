9c0b48d4c10bca4e14d1c94aa02568f367644e7f
Leo Lad <87998262+Dedition@users.noreply.github.com>
2022-07-06 13:40:04 -0400

    Added flask post form

---
+++ app/forms/post_form.py
@@ -0,0 +1,24 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, IntegerField
+from wtforms.validators import DataRequired, ValidationError
+
+
+class CreatePostForm(FlaskForm):
+    user_id = IntegerField('user_id', validators=[DataRequired()])
+    content = StringField('content', validators=[DataRequired()])
+
+    def validate_user_id(self, user_id):
+        if user_id.data < 0:
+            raise ValidationError('User ID must be positive')
+
+    def validate_content(self, content):
+        if len(content.data) > 140:
+            raise ValidationError('Content must be less than 140 characters')
+
+
+class EditPostForm(FlaskForm):
+    content = StringField('content', validators=[DataRequired()])
+
+    def validate_content(self, content):
+        if len(content.data) > 140:
+            raise ValidationError('Content must be less than 140 characters')
