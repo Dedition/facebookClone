27467fe7a14232c83c36f8258aad6ef9498f565e
Leo Lad <87998262+Dedition@users.noreply.github.com>
2022-07-09 03:48:22 -0400

    Update

--- app/forms/post_form.py
+++ app/forms/post_form.py
@@ -8,6 +8,8 @@ class CreatePostForm(FlaskForm):
     content = StringField('content', validators=[DataRequired()])
     image_url = StringField('image_url')
 
+    #! This doesn't work because less than zero is considered None and None isn't considered something
+    #! that can be compared against
     # def validate_user_id(self, user_id):
     #     if user_id.data < 0:
     #         raise ValidationError('User ID must be positive')
@@ -19,6 +21,7 @@ class CreatePostForm(FlaskForm):
 
 class EditPostForm(FlaskForm):
     content = StringField('content', validators=[DataRequired()])
+    image_url = StringField('image_url')
 
     def validate_content(self, content):
         if len(content.data) > 140:
