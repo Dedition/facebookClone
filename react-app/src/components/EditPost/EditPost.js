988b98ca6589481784e875cd85c0dbb9bc139c0f
Leo Lad <87998262+Dedition@users.noreply.github.com>
2022-07-11 20:12:26 -0400

    Added validation handling to all forms on frontend and backend

--- react-app/src/components/EditPost/EditPost.js
+++ react-app/src/components/EditPost/EditPost.js
@@ -1,5 +1,5 @@
 //*                         React
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
 //*                         Store
@@ -9,7 +9,7 @@ import "./EditPost.css";
 function EditPost({ post, isOpen }) {
     const dispatch = useDispatch();
     const [content, setContent] = useState(post.content);
-    const [imageUrl, setImageUrl] = useState(post.image_url);
+    const [errors, setErrors] = useState([]);
 
     const user = useSelector(state => state.session.user);
     let posts = useSelector(state => state.posts);
@@ -31,6 +31,14 @@ function EditPost({ post, isOpen }) {
         isOpen(false);
     }
 
+    useEffect((errors = []) => {
+        if (content.length < 1) errors.push("Content is required to update a post");
+
+        if (content.length > 140) errors.push("Content is too long. Must be less than 140 characters")
+
+        setErrors(errors);
+    }, [content]);
+
     return (
         <div className="editPost">
             <div className='editPost-Header'>
@@ -38,9 +46,14 @@ function EditPost({ post, isOpen }) {
                     {/* <img src={`${user?.avatar}`} alt="avatar" className='header__avatar editPost__avatar' /> */}
                     <h3 style={{ "color": "whitesmoke" }}>Post Contents</h3>
                     <form onSubmit={handleSubmit}>
+                        <ul className="modal-errors">
+                            {errors.map((error, i) => (
+                                <li key={i}>{error}</li>
+                            ))}
+                        </ul>
                         <input className="editPost__input" value={content} onChange={(e) => setContent(e.target.value)} type="text" placeholder={`What's on your mind, ${user?.username}?`} />
                         {/* <input className="editPost__image" placeholder='Image URL (Optional)' value={imageUrl} onChange={imageUrl} /> */}
-                        <button className="editPost__button" type="submit" onClick={handleSubmit}>Hidden</button>
+                        <button className="editPost__button" type="submit" onClick={handleSubmit} disabled={!!errors.length}>Hidden</button>
                     </form>
                 </div>
             </div>
--- react-app/src/store/post.js
+++ react-app/src/store/post.js
@@ -32,6 +32,13 @@ export const createPost = (data) => async (dispatch) => {
     if (response.ok) {
         const newPost = await response.json();
         dispatch(addPost(newPost));
+    } else if (response.status < 500) {
+        const data = await response.json();
+        if (data.errors) {
+            return data.errors;
+        }
+    } else {
+        return ['An error occurred. Please try again.']
     }
 };
 
