a0b79ad9abec01e5545abcd508e1a08b04cd5043
Leo Lad <87998262+Dedition@users.noreply.github.com>
2022-07-27 00:22:48 -0400

    Added a store for the user

---
+++ react-app/src/store/users.js
@@ -0,0 +1,57 @@
+// TODO ——————————————————————————————————————————————————————————————————————————————————
+// TODO                                 Action
+// TODO ——————————————————————————————————————————————————————————————————————————————————
+
+const GET_ALL_USERS = "USER/GET_ALL_USERS";
+const CLEAN_USERS = "USER/CLEAN_USERS";
+
+
+// TODO ——————————————————————————————————————————————————————————————————————————————————
+// TODO                                 Action Creators
+// TODO ——————————————————————————————————————————————————————————————————————————————————
+
+const getUsers = (payload) => ({ type: GET_ALL_USERS, payload });
+export const cleanUsers = () => ({ type: CLEAN_USERS });
+
+// *    ——————————————————————————————————————————————————————————————————————————————————
+// *                                    Thunks
+// *    ——————————————————————————————————————————————————————————————————————————————————
+
+// TODO ——————————————————————————————————————————————————————————————————————————————————
+// TODO                                 READ
+// TODO ——————————————————————————————————————————————————————————————————————————————————
+
+export const getAllUsers = () => async (dispatch) => {
+    const response = await fetch("/api/users");
+
+    if (response.ok) {
+        const users = await response.json();
+
+        dispatch(getUsers(users));
+        return users;
+    }
+};
+
+// TODO ——————————————————————————————————————————————————————————————————————————————————
+// TODO                                 Reducer
+// TODO ——————————————————————————————————————————————————————————————————————————————————
+
+const initialState = {};
+
+const usersReducer = (state = initialState, action) => {
+    const newState = { ...state };
+    switch (action.type) {
+        case GET_ALL_USERS:
+            const users = action.payload;
+            users.forEach((user) => {
+                newState[user.id] = user;
+            });
+            return newState;
+        case CLEAN_USERS:
+            return {};
+        default:
+            return state;
+    }
+};
+
+export default usersReducer;
